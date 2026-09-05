44211cd99398979fadfa933e4b9a26792bc9c23b
DefKorns <defkorns@gmail.com>
2026-09-05 00:11:42 +0000

    Add 8x8 font files for extended Latin and Hiragana characters
    - Created font8x8_ext_latin.h for extended Latin characters (U+00A0 to U+00FF) with 8x8 bitmap representations.
    - Created font8x8_hiragana.h for Hiragana characters (U+3040 to U+309F) with 8x8 bitmap representations.
    - Introduced font8x8_latin.h to include basic, control, extended Latin, and Hiragana fonts for comprehensive character support.

---
+++ src/framework/font8x8.h
@@ -0,0 +1,12 @@
+/** 
+ * 8x8 monochrome bitmap fonts for rendering
+ * Author: Daniel Hepper <daniel@hepper.net>
+ * 
+ * License: Public Domain
+ * 
+ * Based on:
+ * http://dimensionalrift.homelinux.net/combuster/mos3/?p=viewsource&file=/modules/gfx/font8_8.asm
+ **/
+
+#include "font8x8_latin.h"
+#include "font8x8_hiragana.h"
