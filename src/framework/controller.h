863e5adfc4865ce4133ec985108afcddbbc47bb8
DefKorns <defkorns@gmail.com>
2026-09-05 01:42:10 +0000

    Fix formatting of copyright notice in controller.h

--- src/framework/controller.h
+++ src/framework/controller.h
@@ -1,14 +1,13 @@
 /**
-  * Copyright (C) 2017-2018 CompCom
-  *
-  * This program is free software; you can redistribute it and/or
-  * modify it under the terms of the GNU General Public License
-  * as published by the Free Software Foundation; either version 3
-  * of the License, or (at your option) any later version.
-  */
+ * Copyright (C) 2017-2018 CompCom
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ */
 #ifndef CONTROLLER_H_
 #define CONTROLLER_H_
-
 #include <map>
 
 enum GameButton : uint16_t { A=304, B=305, X=307, Y=308, L=310, R=311, SELECT=314, START=315, LEFT=704, RIGHT=705, UP=706, DOWN=707  };
